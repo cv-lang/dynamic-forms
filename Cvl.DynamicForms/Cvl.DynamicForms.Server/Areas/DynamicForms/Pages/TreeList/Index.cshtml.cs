634160794edb3633609d9b7efbe5dac5655a538c
micje32 <jmichaj.mail@gmail.com>
2021-04-06 14:40:11 +0200

    added treeview

---
+++ Cvl.DynamicForms/Cvl.DynamicForms.Server/Areas/DynamicForms/Pages/TreeList/Index.cshtml.cs
@@ -0,0 +1,34 @@
+using System.Linq;
+using Cvl.DynamicForms.Services;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cvl.DynamicForms.Areas.DynamicForms.Pages.TreeList
+{
+    public class IndexModel : PageModel
+    {
+        private readonly DataService dataService;
+        private readonly PropretyGridService viewService;
+
+        public Model.PropertyGridViewModel PropertyGrid { get; set; }
+
+        public IndexModel(DataService dataService, ViewConfigurationService viewConfigurationService)
+        {
+            this.dataService = dataService;
+            this.viewService = new PropretyGridService(dataService, viewConfigurationService);
+        }
+
+        public void OnGet()
+        {
+            var query = Request.Query;
+            var objectIdStr = query["id"].ToString();
+            var objectId = long.Parse(objectIdStr);
+            var type = query["type"];
+
+            var parameters = new Parameters(query.Select(x => new Parameter() { Key = x.Key, Value = x.Value.ToString() }));
+
+            var obj = dataService.GetObject(objectIdStr, type);
+            PropertyGrid = viewService.GetPropertyGrid(obj, parameters);
+
+        }
+    }
+}
