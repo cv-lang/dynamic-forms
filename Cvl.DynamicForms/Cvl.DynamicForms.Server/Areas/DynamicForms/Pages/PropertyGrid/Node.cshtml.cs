b0cddfd79f3e0dac22f4951092ae1171fdf483c2
micje32 <jmichaj.mail@gmail.com>
2021-04-12 14:58:22 +0200

    working on nodes

---
+++ Cvl.DynamicForms/Cvl.DynamicForms.Server/Areas/DynamicForms/Pages/PropertyGrid/Node.cshtml.cs
@@ -0,0 +1,37 @@
+using System.Linq;
+using Cvl.DynamicForms.Model.ViewModel;
+using Cvl.DynamicForms.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Cvl.DynamicForms.Areas.DynamicForms.Pages.PropertyGrid
+{
+    public class PropNodeModel : PageModel
+    {
+        private readonly DataServiceBase dataService;
+        private readonly TreeListService viewService;
+
+        public TreeListViewModel TreeList { get; set; }
+
+        public PropNodeModel(DataServiceBase dataService, ViewConfigurationService viewConfigurationService)
+        {
+            this.dataService = dataService;
+            this.viewService = new TreeListService(dataService, viewConfigurationService);
+        }
+
+        public void OnGet()
+        {
+            var query = Request.Query;
+            var objectIdStr = query["id"].ToString();
+            var type = query["type"];
+
+            var parameters = new Parameters(query.Select(x => new Parameter() { Key = x.Key, Value = x.Value.ToString() }));
+
+            var p = new CollectionViewModelParameters();
+
+
+            TreeList = viewService.GetTreeList(objectIdStr, type, p);
+
+        }
+    }
+}
