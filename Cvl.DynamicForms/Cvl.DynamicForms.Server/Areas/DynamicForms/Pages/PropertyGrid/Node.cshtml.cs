1dc017bb37b7ea7b55720075a93b8a0090c42a48
micje32 <jmichaj.mail@gmail.com>
2021-04-13 11:14:27 +0200

    fixing prop nodes

--- Cvl.DynamicForms/Cvl.DynamicForms.Server/Areas/DynamicForms/Pages/PropertyGrid/Node.cshtml.cs
+++ Cvl.DynamicForms/Cvl.DynamicForms.Server/Areas/DynamicForms/Pages/PropertyGrid/Node.cshtml.cs
@@ -1,7 +1,5 @@
 using System.Linq;
-using Cvl.DynamicForms.Model.ViewModel;
 using Cvl.DynamicForms.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Cvl.DynamicForms.Areas.DynamicForms.Pages.PropertyGrid
@@ -9,28 +7,27 @@ namespace Cvl.DynamicForms.Areas.DynamicForms.Pages.PropertyGrid
     public class PropNodeModel : PageModel
     {
         private readonly DataServiceBase dataService;
-        private readonly TreeListService viewService;
+        private readonly PropretyGridService viewService;
 
-        public TreeListViewModel TreeList { get; set; }
+        public Model.PropertyGridViewModel PropertyGrid { get; set; }
 
         public PropNodeModel(DataServiceBase dataService, ViewConfigurationService viewConfigurationService)
         {
             this.dataService = dataService;
-            this.viewService = new TreeListService(dataService, viewConfigurationService);
+            this.viewService = new PropretyGridService(dataService, viewConfigurationService);
         }
 
         public void OnGet()
         {
             var query = Request.Query;
             var objectIdStr = query["id"].ToString();
+            var objectId = long.Parse(objectIdStr);
             var type = query["type"];
 
             var parameters = new Parameters(query.Select(x => new Parameter() { Key = x.Key, Value = x.Value.ToString() }));
 
-            var p = new CollectionViewModelParameters();
-
-
-            TreeList = viewService.GetTreeList(objectIdStr, type, p);
+            var obj = dataService.GetObject(objectIdStr, type);
+            PropertyGrid = viewService.GetPropertyGrid(obj, parameters);
 
         }
     }
